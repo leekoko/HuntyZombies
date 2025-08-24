5432a7b5602310f497534cc6af4a6a8337446c9c
yubin lee <908230189@qq.com>
2025-08-24 21:24:49 +0800

    更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-92ZV67NPW5"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-92ZV67NPW5');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
